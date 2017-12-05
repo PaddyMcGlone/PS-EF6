0dddbedd88c1c485f635e024769895dd60b7bfd7
paddymcglone <paddymcglone@gmail.com>
2017-12-05 20:52:05 +0000

    Adding the DbContext and DbSets into the context

---
+++ NinjaDomain.Classes/NinjaDomain.DataModel/Class1.cs
@@ -0,0 +1,14 @@
+﻿using NinjaDomain.Classes;
+using System.Data.Entity;
+
+namespace NinjaDomain.DataModel
+{
+    public class NinjaContext : DbContext
+    {
+        public DbSet<Ninja> Ninjas { get; set; }
+
+        public DbSet<NinjaEquipment> NinjaEquipments { get; set; }
+
+        public DbSet<Clan> Clans { get; set; }
+    }
+}
